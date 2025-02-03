4355270824820542e22689d907ec16a803c38130
KushMomaya <kushmomaya@gmail.com>
2025-02-02 23:14:55 -0800

    function headers and skeleton for general seach

--- algo.py
+++ algo.py
@@ -1,6 +1,32 @@
 import heapq as q
+from node import Node
 
 
-def general_search(puzzle, queueing_function):
+def general_search(initial_state, queueing_function):
     nodes = []
-    q.heappush(nodes, )
+    initial_node = Node(state=initial_state)
+    q.heappush(nodes, initial_node)
+
+    while nodes:
+        if len(nodes) == 0:
+            return "failure"
+        
+        node = q.heappop(nodes)
+
+        #if check goal state
+            #then return node
+        neighbors = #expand(node, operators)
+        nodes = queueing_function(nodes, #neighbors)
+                                  
+
+def expand(node, operators):
+
+def uniform_cost(nodes, neighbors):
+
+def astar_manhattan(nodes, neighbors):
+
+def astar_misplaced(nodes, neighbors):
+
+def tile_check(state):
+
+def distance_check(state):
