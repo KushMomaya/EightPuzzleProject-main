a0a84d9c4a1394e4b3f825163d85a6fdc9c38b5c
KushMomaya <kushmomaya@gmail.com>
2025-02-02 23:35:49 -0800

    misplaced tile function, less than node override, comments

--- algo.py
+++ algo.py
@@ -1,11 +1,13 @@
 import heapq as q
 from node import Node
 
-
-def general_search(initial_state, queueing_function):
+# General Search Template
+def general_search(initial_state, goal_state, queueing_function):
     nodes = []
     initial_node = Node(state=initial_state)
     q.heappush(nodes, initial_node)
+
+    seen = set()
 
     while nodes:
         if len(nodes) == 0:
@@ -13,20 +15,38 @@ def general_search(initial_state, queueing_function):
         
         node = q.heappop(nodes)
 
-        #if check goal state
-            #then return node
-        neighbors = #expand(node, operators)
-        nodes = queueing_function(nodes, #neighbors)
+        if node.state == goal_state:
+            return node
+        operators = ["left", "right", "up", "down"]
+        neighbors = expand(node, operators)
+        nodes = queueing_function(nodes, neighbors)
                                   
-
+# Expand function returns an array of valid neighbors for the node passed in
 def expand(node, operators):
 
+# Queueing logic for uniform cost search
 def uniform_cost(nodes, neighbors):
 
+# Queueing logic for A* with the manhattan distance heuristic
 def astar_manhattan(nodes, neighbors):
 
+# Queueing logic for A* with the misplaced tile heuristic
 def astar_misplaced(nodes, neighbors):
 
+# Check the current amount of tiles in the wrong position    
 def tile_check(state):
+    count = 0
+    goal_state = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,0]
+    ]
+    for i in range(3):
+        for j in range(3):
+            if state[i,j] != goal_state[i][j]:
+                count = count + 1
+    
+    return count
 
+# Check the total distance of each tile from its current state
 def distance_check(state):
--- node.py
+++ node.py
@@ -7,4 +7,7 @@ class Node:
         self.heuristic = heuristic # Heuristic: distance of the current node to the goal state
         self.f = cost + heuristic # g(n) + h(n), comparing the cost of the node with the distance to reach the goal state to decide where to expand
 
-    
+
+    # Override the less than operator (<) so I can directly compare the f value of nodes when deciding where to go
+    def __lt__(self, other):
+        return self.f < other.f
